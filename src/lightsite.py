2f54503708c15805358b1df488b8cb3a4d47a53c
goneall <gary@sourceauditor.com>
2014-11-06 11:01:25 -0800

    Just starting to flesh out the site

--- src/lightsite.py
+++ src/lightsite.py
@@ -1,18 +1,19 @@
 from logging import handlers
 import logging
-from flask import Flask
-from flask import render_template
+from flask import Flask, url_for, render_template
 LOGFILE_NAME = '/var/log/lightsite/lightsite.log'
-
+#LOGFILE_NAME = 'lightsite.log'
+WEB_ROUTE = '/lights'    # Web routing to the light site application
 app = Flask(__name__)
+#app.debug=True
 # Enable logging
 file_handler = handlers.RotatingFileHandler(LOGFILE_NAME)
 file_handler.setLevel(logging.DEBUG)
 app.logger.addHandler(file_handler)
 
-@app.route("/")
-def hello(name=None):
-  return render_template('lightsite.html')
+@app.route(WEB_ROUTE)
+def hello():
+    return render_template('lightsite.html', lightOn=False)
 
 if __name__ == "__main__":
-  app.run('0.0.0.0')
+    app.run('0.0.0.0')
