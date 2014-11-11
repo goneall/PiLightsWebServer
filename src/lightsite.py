71674b4d5592af79e86882bb5fceaf5f189ef3e1
goneall <gary@sourceauditor.com>
2014-11-10 18:23:11 -0800

    Getting there - intermediate checkin

--- src/lightsite.py
+++ src/lightsite.py
@@ -13,6 +13,7 @@ DATABASE = 'db'
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.secret_key = '\xf4\x9a\x8f\x08\xd6\xedg\xe4W1f\x87\x1a\x9al\xfa\x90\xb2!"R0b\x10'
 
 app.debug=app.config['DEBUG']
 # Enable logging
@@ -45,25 +46,75 @@ def teardown_request(exception):
         db.close()
 
 @app.route(app.config['WEB_ROUTE_MAIN'])
-def hello():
-    return render_template('lightsite.html', lightOn=False)
+def lightstatus():
+    return render_template('lightsite.html', lightson=False, playliston=True)
 
+@app.route(app.config['WEB_ROUTE_MAIN'] + '/update', methods=['POST'])
+def updatelights():
+    if (request.form['lightstatus'] == u'lightson'):
+        lightson = True # replace with call to library
+    elif (request.form['lightstatus'] == u'playliston'):
+        playliston = True
+    else:
+        alloff = True
+    return redirect(url_for('lightstatus'))
+    
 @app.route(app.config['WEB_ROUTE_SCHED'])
 def schedule():
     cursor = g.db.execute('select id, day, hour, minute, turnon, turnoff, startplaylist, stopplaylist from schedule order by id')
-    entries = []
-    for row in cursor.fetchall():
-        entries[0] = dict(id=row[0], day=row[1], minute=row[2], turnon=row[3], startplaylist=row[4], stoplaylist=row[5])
-    return render_template('schedule.html')
+    entries = [dict(id=row[0], day=row[1], hour=row[2], minute=row[3], turnon=row[4], turnoff=row[5], 
+                    startplaylist=row[6], stopplaylist=row[7]) for row in cursor.fetchall()]
+    return render_template('schedule.html', entries=entries)
 
-@app.route(app.config['WEB_ROUTE_MAIN']+'/add', methods=['POST'])
+@app.route(app.config['WEB_ROUTE_MAIN']+'/schedule/add', methods=['POST'])
 def add_schedule():
-    g.db.execute('insert into schedule (day, hour, minute, turnon, turnoff, startplaylist, stopplayist) values (?, ?, ?, ?, ?, ?, ?)',
-                 request.form['day'], request.form['hour'], request.form['minute'], request.form['turnon'],
-                 request.form['startplaylist'], request.form['stopplaylist'])
-    g.db.commit()
-    flash('Schedule updated')
+    try:
+        action = request.form['action']
+        turnon = action == 'turnon'
+        turnoff = action == 'turnoff'
+        startplaylist = action == 'startplaylist'
+        stopplaylist = action == 'stopplaylist'
+        time = '0:0'
+        if ('time' in request.form):
+            time = request.form['time']
+        timep = time.split(':')
+        hour = timep[0]
+        minute = 0
+        if (len(timep) > 1):
+            minute = timep[1]
+        g.db.execute('insert into schedule (day, hour, minute, turnon, turnoff, startplaylist, stopplaylist) values (?, ?, ?, ?, ?, ?, ?)',
+                     [request.form['day'], hour, minute, turnon,
+                      turnoff, startplaylist, stopplaylist])
+        flash('Schedule updated')
+    except Exception as ex:
+        logging.error('Error adding new schedule record: '+ex.message)
+        flash('Error adding schedule')
+    finally:
+        g.db.commit()
     return redirect(url_for('schedule'))
+
+@app.route(app.config['WEB_ROUTE_MAIN']+'/schedule/delete', methods=['POST'])
+def delete_schedule():
+    schedule_id = int(request.form['delete_entry'])
+    try:
+        g.db.execute('delete from schedule where id=?', [schedule_id])
+        flash('Schedule item deleted')
+    except Exception as ex:
+        logging.error('Error deleting schedule record '+str(schedule_id)+':'+ ex.message)
+        flash('Error deleting schedule record')
+    finally:
+        g.db.commit
+    return redirect(url_for('schedule'))
+        
+@app.route(app.config['WEB_ROUTE_MAIN']+'/playlist')
+def playlist():
+    current_playlist = ['need to fill in', 'second song.mp3']
+    return render_template('playlist.html', playlist=current_playlist)
+
+@app.route(app.config['WEB_ROUTE_MAIN']+'/playlist/add')
+def add_song():
+    flash('Song Added')
+    return redirect(url_for('playlist'))
 
 if __name__ == "__main__":
     
