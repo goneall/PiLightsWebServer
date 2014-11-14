51ff00b0252764f7b020b0712a89f244aa362d1f
goneall <gary@sourceauditor.com>
2014-11-13 22:04:05 -0800

    Added missing files

---
+++ src/adduser.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# Licensed under the Apache 2.0 License
+'''
+Add a user to the database
+Usage: adduser username password
+Created on Nov 13, 2014
+
+@author: Gary O'Neall
+'''
+import sys
+import sqlite3
+from hashlib import sha256
+from os import path
+
+DB_PATH = 'src/db'
+
+def usage():
+    ''' Pprints the usage to the console
+    '''
+    print "Usage:"
+    print "adduser username password"
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        usage()
+        sys.exit(1)
+    username = sys.argv[1].strip()
+    password = sys.argv[2].strip()
+    password_hash = sha256(password)
+    password_dig = password_hash.hexdigest()
+    if not path.isfile(DB_PATH):
+        print "Database is not initialized"
+        sys.exit(1)
+    con = sqlite3.connect(DB_PATH)
+
+    try:
+        cursor = con.execute('select id from users where username=?', [username])
+        row = cursor.fetchone()
+        if row:
+            print "User already exists"
+            sys.exit(1)
+        con.execute('insert into users (username, password) values (?, ?)', [username, password_dig])
+        print 'user added'
+    except Exception as ex:
+        print "Error updating database: "+str(ex)
+    finally:
+        con.commit()
+        con.close()
---
+++ src/deleteuser.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Licensed under the Apache 2.0 License
+'''
+Deletes a user from the database
+Usage: deleteuser username
+Created on Nov 13, 2014
+
+@author: Gary O'Neall
+'''
+import sys
+import sqlite3
+from hashlib import sha256
+from os import path
+
+DB_PATH = 'src/db'
+
+def usage():
+    ''' Prints the usage to the console
+    '''
+    print "Usage:"
+    print "deleteuser username"
+    
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+    username = sys.argv[1].strip()
+    if not path.isfile(DB_PATH):
+        print "Database is not initialized"
+        sys.exit(1)
+    con = sqlite3.connect(DB_PATH)
+
+    try:
+        cursor = con.execute('select id from users where username=?', [username])
+        row = cursor.fetchone()
+        if not row:
+            print "User does not exist"
+            sys.exit(1)
+        con.execute('delete from users where username=?', [username])
+        print 'user deleted'
+    except Exception as ex:
+        print "Error updating database: "+str(ex)
+    finally:
+        con.commit()
+        con.close()
