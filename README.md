bcbcaa1dc8d44278e187e9934932e917dd2ff395
goneall <gary@sourceauditor.com>
2014-11-14 11:55:06 -0800

    Added interface to LightShowPi

--- README.md
+++ README.md
@@ -7,6 +7,19 @@ Usaage:
 ./lightsite.py
 Starts up the web server on port 5000
 
+The webserver can be configured with a configuration file in the environment variable PILIGHTSWEB_SETTING.
+
+Following are the configuration variables:
+* PORT - Port number for the webserver.  Default 5000.
+* WEB_ROUTE_MAIN - Web routing for the main application.  Default '/lights'    
+* WEB_ROUTE_SCHED - Web routing for the scheduling application.  Default WEB_ROUTE_MAIN + '/schedule'  
+* WEB_ROUTE_PLAYLIST - Web routing for the playlist management app.  Default WEB_ROUTE_MAIN + '/playlist'
+* DATABASE - File name of the SQLite database to hold user, schedule, and playlist information.  Default 'db'
+* MUSIC_PATH - File path to the directory containing music MP3 files.  Default '/home/pi/music'
+* DEBUG - If true, enable debug mode.  Default True
+
+PiLightsWebServer uses the Apache 2.0 license (see LICENSE for text).
+
 This web server uses (and depends on) Flask.
 
 Flask can be installed by entering the following:
--- src/lightsinterface.py
+++ src/lightsinterface.py
@@ -4,32 +4,79 @@ Created on Nov 11, 2014
 @author: Gary O'Neall
 Licensed under the Apache 2.0 License
 
-Interface module for the lightpi
+Interface module for the lightshowPi
 Currently, these are just stubs used to test the web interface
 '''
+from configuration_manager import songs, set_songs
+from hardware_controller import turn_on_lights, turn_off_lights, initialize
 
-# TODO: Replace with a call to playlist
-playlist = [{'playorder':1, 'name':'name1', 'path':'path1'}, {'playorder':2, 'name':'name2', 'path':'path2'}]
+lights_are_on = False
+playlist_playing = False
+initialized = False
 
 def getplaylist():
-    #TODO: Implement
+    current_songs = songs()
+    # Format of the songs is name, path, number of votes
+    # Need to convert the number of votes into a play order
+    sorted(current_songs, key=lambda song: len(song[2]), reverse=True)
+    playlist = []
+    order = 1
+    for song in current_songs:
+        playlist.append({'playorder':order, 'name':song[0], 'path':song[1]})
+        order = order + 1
     return playlist
 
 def update_playlist(updated_playlist):
-    #TODO Implement
-    playlist = updated_playlist
+    new_songs = []
+    sorted(updated_playlist, key=lambda item: item.playorder)
+    max_order = updated_playlist[len(updated_playlist)-1].playorder
+    for item in updated_playlist:
+        # Hack alert - I don't really understand the votes and what information we
+        # are loosing here
+        # Create a set the same size as the list size - play order
+        vote_set = set()
+        num_votes = max_order - item.playorder
+        for vote in range(0, num_votes):
+            vote_set.add(str(vote))
+        new_songs.append([item.name, item.path, vote_set])
+    set_songs(new_songs)
 
 def lights_on():
-    #TODO: Implement
-    lisghtson=True
+    global lights_are_on, playlist_playing
+    initialize_interface()
+    if playlist_playing:
+        stop_playlist()
+    turn_on_lights()
+    lights_are_on = True
 
 def lights_off():
-    #TODO: Implement
-    lightsoff=True
+    global lights_are_on, playlist_playing
+    initialize_interface()
+    if playlist_playing:
+        stop_playlist()
+    turn_off_lights()
+    lights_are_on = False
 
 def start_playlist():
-    #TODO Implement
-    startPlaylist=True
+    global lights_are_on, playlist_playing
+    initialize_interface()
+    if lights_are_on:
+        lights_off()
+    #TODO: Implement
+    
+    
+def stop_playlist():
+    global lights_are_on, playlist_playing
+    initialize_interface()
+    if lights_are_on:
+        lights_off()
+        
+def initialize_interface():
+    global initialized
+    if not initialized:
+        initialize()
+        initialized = True
+    #TODO: Implement
     
 if __name__ == "__main__":
     print 'This module intefaces to the lightshowpi modules'
