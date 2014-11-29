43390d030bc677b8ab2fa9baa8bee12252394087
Gary O'Neall <gary@sourceauditor.com>
2014-11-28 20:23:11 -0800

    Remove the crs

--- src/playAllPlaylist.sh
+++ src/playAllPlaylist.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#
-# Plays the entire playlist.  Single parameter, playlist file
-
+# Plays entire playlist - single parameter playlist name
 while true; do
-  sudo python $SYNCHRONIZED_LIGHTS_HOME/py/synchronized_lights.py $1
+   sudo python $SYNCHRONIZED_LIGHTS_HOME/py/synchronized_lights.py $1
 done
